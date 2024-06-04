6a39d2d79679a0b7eb0809f4bf15b54399fc0f88
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-06-04 15:57:35 +0300

    Modify the form based on login & register

--- frontend/app/components/authForm.jsx
+++ frontend/app/components/authForm.jsx
@@ -1,7 +1,8 @@
 import {styles} from "./authFormStyles"
 
 import { View, Text, TextInput, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { router } from 'expo-router'
 
 import  globalStyles  from '../globalStyles';
 import CustomButton from './customButton';
@@ -28,67 +29,89 @@ const FormField = ({title, value, placeholder, onFormChange, formValues, setForm
   )
 }
 
-const AuthForm = ({title, value, placeholder, onFormChange, onLogin, googleAuth, facebookAuth,  ...props}) => {
-  let errorMsg = '';
-  const [formValues, setFormValues] = React.useState({
-    email: '',
-    password: '',
-  })
-  const handleChangeText = (e) => {
-    console.log(e.value)
-  }
-
-  return (
-    <>
-      <FormField title={"Е-Поща"} type={'email'} onFormChange={onFormChange} formValues={formValues} setFormValues={setFormValues} />
-      <FormField title={"Парола"} type={'password'} onFormChange={onFormChange} formValues={formValues} setFormValues={setFormValues} />
-      <Text style={styles.errorMsg}>{errorMsg}</Text>
-      <CustomButton title={'Влез'} additionalStyles={{marginTop: 15,}} handlePress={onLogin}/>
-      <Text style={{fontSize: 14, fontWeight: 'bold', textTransform: 'uppercase'}}>или</Text>
-      <View style={styles.thirdPartyAuthBox}>
-        <CustomButton 
-        title={'Влез с Google'} 
-        handlePress={googleAuth}
-        additionalStyles={{ 
-          width: '35%',
-          padding: 5,
-          // borderRadius: 30, 
-          backgroundColor: '#FFF',
-          borderColor: globalStyles.primaryColor,
-          borderWidth: 1, 
-        }}
-        imagePath={GooglePath}
-          />
-        <CustomButton 
-        title={'Влез с Facebook'} 
-        handlePress={facebookAuth}
-        additionalStyles={{
-          width: '35%',
-          padding: 5,
-          // borderRadius: 30, 
-          backgroundColor: '#FFF',
-          borderColor: globalStyles.primaryColor,
-          borderWidth: 1, 
-        }}
-        imagePath={FacebookPath}
+const AuthForm = ({
+    title, 
+    value, 
+    placeholder, 
+    onFormChange, 
+    onLogin, 
+    googleAuth, 
+    facebookAuth, 
+    isReg, 
+    onRegister,  
+    ...props
+  }) => {
+    let errorMsg = 'Error Message Example';
+    const [formValues, setFormValues] = React.useState({
+      // name: '',
+      email: '',
+      password: '',
+      repeatPassword: ''
+    })
+    useEffect(() => {
+      isReg ? null : setFormValues(prev => prev = { email: '', password: '' })
+    }, [])
+  
+    return (
+      <>
+        {/* {isReg && <FormField title={"Име"} type={'name'} onFormChange={onFormChange} formValues={formValues} setFormValues={setFormValues} />} */}
+        <FormField title={"Е-Поща"} type={'email'} onFormChange={onFormChange} formValues={formValues} setFormValues={setFormValues} />
+        <FormField title={"Парола"} type={'password'} onFormChange={onFormChange} formValues={formValues} setFormValues={setFormValues} />
+        {isReg && <FormField title={"Повтори парола"} type={'repeatPassword'} onFormChange={onFormChange} formValues={formValues} setFormValues={setFormValues} />}
+        <Text style={styles.errorMsg}>{errorMsg}</Text>
+        {
+        isReg 
+        ? <CustomButton title={'Създай'} additionalStyles={{marginTop: 15,}} handlePress={onRegister}/> 
+        : <CustomButton title={'Влез'} additionalStyles={{marginTop: 15,}} handlePress={onLogin}/>
+        }
+        <Text style={{fontSize: 14, fontWeight: 'bold', textTransform: 'uppercase'}}>или</Text>
+        <View style={styles.thirdPartyAuthBox}>
+          <CustomButton 
+          title={'Влез с Google'} 
+          handlePress={googleAuth}
+          additionalStyles={{ 
+            width: '35%',
+            padding: 5,
+            // borderRadius: 30, 
+            backgroundColor: '#FFF',
+            borderColor: globalStyles.primaryColor,
+            borderWidth: 1, 
+          }}
+          imagePath={GooglePath}
             />
-      </View>
-      <View>
-      <TouchableOpacity >
-        <Text style={styles.link}>
-          Забравихте си паролата? <Text onPress={() => console.log('click')} style={styles.linkBold}>Смени парола.</Text>
-        </Text>
-      </TouchableOpacity>
-      </View>
-      <View>
-      <TouchableOpacity >
-        <Text style={styles.link}>
-          Нямате профил? <Text onPress={() => console.log('click')} style={styles.linkBold}>Създай профил.</Text>
-        </Text>
-      </TouchableOpacity>
-      </View>
-    </>
-  )
+          <CustomButton 
+          title={'Влез с Facebook'} 
+          handlePress={facebookAuth}
+          additionalStyles={{
+            width: '35%',
+            padding: 5,
+            // borderRadius: 30, 
+            backgroundColor: '#FFF',
+            borderColor: globalStyles.primaryColor,
+            borderWidth: 1, 
+          }}
+          imagePath={FacebookPath}
+              />
+        </View>
+        {!isReg && 
+        <>
+        <View>
+        <TouchableOpacity >
+          <Text style={styles.link}>
+            Забравихте си паролата? <Text onPress={() => console.log('click')} style={styles.linkBold}>Смени парола.</Text>
+          </Text>
+        </TouchableOpacity>
+        </View>
+        <View>
+        <TouchableOpacity >
+          <Text style={styles.link}>
+            Нямате профил? <Text onPress={() => router.push('signUp')} style={styles.linkBold}>Създай профил.</Text>
+          </Text>
+        </TouchableOpacity>
+        </View>
+        </>}
+      </>
+    )
 }
 
 export default AuthForm

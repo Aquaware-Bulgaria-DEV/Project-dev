fb692b88244cb230a64948c523c62b6fbd6b04cf
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-06-05 17:57:44 +0300

    get auth token from context

--- frontend/app/(auth)/signUp/index.jsx
+++ frontend/app/(auth)/signUp/index.jsx
@@ -5,6 +5,7 @@ import { Image } from 'expo-image';
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import AuthForm from '../../components/authForm'
+import AuthContext from '../../Context/AuthContext';
 
 import {styles} from "./sign-upStyles"
 
@@ -12,17 +13,30 @@ import AquawareLogo from "../../../assets/AquawareLogo.svg"
 
 const SignUp = () => {
   const [formValues, setFormValues] = React.useState({ name: '', email: '', password: '', repeatPassword: ''});
-
+  const {
+    token,
+    preferences,
+    getToken,
+    saveToken,
+    removeToken,
+    savePreferences,
+    removePreferences,
+  } = React.useContext(AuthContext);
+  
   const handleFormChange = (newValues) => {
     setFormValues(newValues);
     console.log(newValues)
   };
-
+  
   const handleRegister = () => {
-
+    
     // Here goes the register by Mario
-
+    // let newToken = {email: 'notebook', password: 'b', repeatPassword: 'b'};
+    saveToken(token)
     console.log('Register with:', formValues);
+    console.log(`Authorization token is ${JSON.stringify(token)}`)
+    console.log(`Preferences token is ${preferences}`)
+    removeToken()
   };
 
   const googleHandler = () => {

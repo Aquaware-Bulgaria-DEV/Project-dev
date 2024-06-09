157b6745aa1aa9ffcc020076a9d95fc58faea681
vaninaNina <kalapsazova.vanina@gmail.com>
2024-06-09 16:13:08 +0300

    create a tip screen

--- frontend/app/_layout.jsx
+++ frontend/app/_layout.jsx
@@ -1,17 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View, StyleSheet } from 'react-native';
-import {Link , Redirect, Stack} from 'expo-router';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import { Text, View, StyleSheet } from "react-native";
+import { Link, Redirect, Stack } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const StackLayout =()=> {
+const StackLayout = () => {
   return (
     <SafeAreaProvider>
       <Stack>
         <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="tip" />
       </Stack>
     </SafeAreaProvider>
   );
-}
+};
 
 // const styles = StyleSheet.create({
 //   safeArea: {
---
+++ frontend/app/tip/tipStyle.js
@@ -0,0 +1,37 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F9F9F9",
+  },
+  scrollViewContent: {
+    paddingBottom: 125,
+    marginTop: 35,
+  },
+  content: {
+    marginHorizontal: 20,
+  },
+  pic: {
+    height: 190,
+    width: "100%",
+    justifyContent: "center",
+    alignSelf: "center",
+    margin: 25,
+    borderRadius: 8,
+  },
+  title: {
+    fontFamily: "Muli",
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 10,
+    marginHorizontal: 10,
+  },
+  text: {
+    fontSize: 19,
+    marginHorizontal: 10,
+    letterSpacing: 0.1,
+    lineHeight: 25,
+  },
+});

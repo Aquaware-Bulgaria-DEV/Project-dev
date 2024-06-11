faba7f0476a1075565a1bdc96cc3894458a767ef
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-06-11 23:37:59 +0300

    possible font family prop
    TODO: add the family

--- frontend/app/globalStyles.js
+++ frontend/app/globalStyles.js
@@ -1,7 +1,9 @@
-const primaryColor = 'rgb(0, 122, 255)';
+<link href="https://fonts.cdnfonts.com/css/muli" rel="stylesheet"></link>
 
+const primaryColor = 'rgb(0, 122, 255)';
+// const primaryFont = "'Muli', sans-serif";
 
 export default globalStyles = {
     primaryColor,
-
+    // primaryFont,
 }

15d26f92a4bf22ef170eb41ad6156bf315c210c0
vaninaNina <kalapsazova.vanina@gmail.com>
2024-06-23 13:37:21 +0300

    rename components folder to globalComponents folder (for reusable components)

--- frontend/app/globalComponents/settingsButton.jsx
+++ frontend/app/globalComponents/settingsButton.jsx
@@ -2,15 +2,17 @@ import { View, Text, Pressable } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { styles } from '../(tabs)/settings/settingsStyles.js';
 import React, { useState } from 'react';
+import { useRouter } from 'expo-router';
 
-const SettingsButton = ({ style, title }) => {
+const SettingsButton = ({ style, title, screen }) => {
+  const router = useRouter();
   const [isPressed, setIsPressed] = useState(false);
   return (
     <Pressable
       onPressIn={() => setIsPressed(true)}
       onPressOut={() => setIsPressed(false)}
       style={style}
-      onPress={() => console.log('redirect')}
+      onPress={() => router.push(screen)}
     >
       {isPressed ? (
         <LinearGradient colors={['#388FED', '#4C62C7']} style={styles.gradient}>

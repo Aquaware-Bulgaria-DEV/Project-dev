78fcfa1a223afef48068f5956e598f1dce2cfd92
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-06-26 21:11:28 +0300

    Update index.jsx

--- frontend/app/subscreens/formSelfReport/index.jsx
+++ frontend/app/subscreens/formSelfReport/index.jsx
@@ -1,75 +1,83 @@
-import { View, Text, ScrollView, Pressable } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Pressable } from 'react-native';
+import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-
-import {  styles  } from "./formSelfReportStyles.js"
+import { styles } from "./formSelfReportStyles.js";
 import { Header } from '../../globalComponents/header.jsx';
-
-import { Picker } from '@react-native-picker/picker';
 import RNPickerSelect from 'react-native-picker-select';
-
+import WaterMeter from '../../globalComponents/waterMeter.jsx';
+import getIcon from '../../../utils/icons.js';
 
 const selfReport = () => {
-    const [value, setValue] = React.useState('');
-    const testData = ["Имот 1", "Имот 2", "Имот 3", "Имот 4", "Имот 5"]
+    const [value, setValue] = useState('');
+    const [opacity, setOpacity] = useState(1);
+    const [meterCount, setMeterCount] = useState(1);
+    const [meters, setMeters] = useState([]);
 
-    const pickerItems = testData.map((key) => ({
+    const dataHouses = ["Имот 1", "Имот 2", "Имот 3", "Имот 4", "Имот 5"];
+    const dataWaterMeter = ["Водомер 1", "Водомер 2"];
+
+    const houseItems = dataHouses.map((key) => ({
         label: key,
         value: key
     }));
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView
-        style={{ flex: 1, }}
-        contentContainerStyle={styles.scrollViewContent}
-        showsVerticalScrollIndicator={false}
-      >
-      <Header showProfilePic={false} />
-        <View style={styles.contentContainer}>
-        <Text style={styles.title}>Добавяне на данни</Text>
-            <Text style={styles.labels}>Имот</Text>
-            <View style={styles.pickerContainer}>
-                {/* <Picker
-                    selectedValue={value}
-                    itemStyle={styles.pickerItem}
-                    onValueChange={(itemValue, itemIndex) => {
-                        // formValues.issue = itemValue;
-                        setValue(itemValue);
-                        
-                        // if (
-                        //   formValues.issue !== '' &&
-                        //   formValues.address !== '' &&
-                        //   formValues.content !== ''
-                        // ) {
-                        //   setError('');
-                        // }
-                    }}
-                    >
-                    {value === '' && (
-                        <Picker.Item label='Избери имот' value='' />
-                    )}
-                    {testData.map((key) => <Picker.Item  key={key} label={key} value={key} />)}
-                </Picker> */}
-                 <RNPickerSelect
-                            onValueChange={(value) => setValue(value)}
-                            items={pickerItems}
-                            style={{
-                                inputIOS: styles.pickerItem,
-                                inputAndroid: styles.pickerItem,
-                            }}
-                            placeholder={{
-                                label: 'Избери имот',
-                                value: '',
-                            }}
-                            value={value}
-                        />
-            </View>
+    const waterItems = dataWaterMeter.map((key) => ({
+        label: key,
+        value: key
+    }));
 
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+    const addWaterMeter = () => {
+        if (meterCount < 5) {
+            setMeters([...meters, <WaterMeter key={meterCount} waterMeters={waterItems} setValue={setValue} value={value} />]);
+            setMeterCount(meterCount + 1);
+        }
+    };
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <ScrollView
+                style={styles.scrollView}
+                contentContainerStyle={styles.scrollViewContent}
+                showsVerticalScrollIndicator={false}
+            >
+                <Header showProfilePic={false} />
+                <View style={styles.contentContainer}>
+                    <Text style={styles.title}>Добавяне на данни</Text>
+                    <View>
+                        <Text style={styles.labels}>Имот</Text>
+                        <View style={styles.pickerContainer}>
+                            <RNPickerSelect
+                                onValueChange={(value) => setValue(value)}
+                                items={houseItems}
+                                style={{
+                                    inputIOS: styles.pickerItem,
+                                    inputAndroid: styles.pickerItem,
+                                }}
+                                placeholder={{
+                                    label: 'Избери имот',
+                                    value: '',
+                                }}
+                                value={value}
+                            />
+                        </View>
+                    </View>
+                    <View style={styles.waterMeterContainer}>
+                        <WaterMeter key={meterCount} waterMeters={waterItems} setValue={setValue} value={value} />
+                        {meters}
+                    </View>
+                    <Pressable
+                        onPress={addWaterMeter}
+                        onPressIn={() => setOpacity(0.5)}
+                        onPressOut={() => setOpacity(1)}
+                    >
+                        <Text style={[styles.addWaterMeter, { opacity }]}>
+                            {getIcon('plus', '#3CA5D8', 15)}Добави водомер
+                        </Text>
+                    </Pressable>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
+};
 
 export default selfReport;

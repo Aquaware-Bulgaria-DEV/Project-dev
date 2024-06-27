795504012c8320cb6c5d87b8438aa5fa3d83efae
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-06-27 22:36:19 +0300

    Update index.jsx

--- frontend/app/subscreens/formSelfReport/index.jsx
+++ frontend/app/subscreens/formSelfReport/index.jsx
@@ -1,44 +1,62 @@
 import { View, Text, ScrollView, Pressable } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { styles } from "./formSelfReportStyles.js";
 import { Header } from "../../globalComponents/header.jsx";
 import RNPickerSelect from "react-native-picker-select";
 import WaterMeter from "../../globalComponents/waterMeter.jsx";
 import getIcon from "../../../utils/icons.js";
+import CustomButton from "../../globalComponents/customButton.jsx";
 
 const selfReport = () => {
   const [value, setValue] = useState("");
   const [opacity, setOpacity] = useState(1);
-  const [meterCount, setMeterCount] = useState(1);
+  const [meterCount, setMeterCount] = useState(2);
   const [meters, setMeters] = useState([]);
   const [formData, setFormData] = useState({});
 
   const dataHouses = ["Имот 1", "Имот 2", "Имот 3", "Имот 4", "Имот 5"];
   const dataWaterMeter = ["Водомер 1", "Водомер 2"];
 
-  const houseItems = dataHouses.map(key => ({
+  const houseItems = dataHouses.map((key) => ({
     label: key,
-    value: key
+    value: key,
   }));
 
-  const waterItems = dataWaterMeter.map(key => ({
+  const waterItems = dataWaterMeter.map((key) => ({
     label: key,
-    value: key
+    value: key,
   }));
 
-  const handleValueChange = value => {
-    setValue(value);
-    if (!formData[value]) {
+  useEffect(() => {
+    if (value) {
+      const meterKey = `meter1`;
+      setMeters([
+        <WaterMeter
+          key={meterKey}
+          waterMeters={waterItems}
+          setFormData={setFormData}
+          formData={formData}
+          houseKey={value}
+          meterKey={meterKey}
+        />,
+      ]);
       setFormData({
         ...formData,
-        [value]: {}
+        [value]: {
+          [meterKey]: {},
+        },
       });
+      setMeterCount(2);
     }
+  }, [value]);
+
+  const handleValueChange = (value) => {
+    setValue(value);
   };
 
   const addWaterMeter = () => {
-    if (meterCount < 5 && value) {
+    if (meterCount <= 5 && value) {
       const meterKey = `meter${meterCount}`;
       setMeters([
         ...meters,
@@ -49,12 +67,23 @@ const selfReport = () => {
           formData={formData}
           houseKey={value}
           meterKey={meterKey}
-        />
+        />,
       ]);
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        [value]: {
+          ...prevFormData[value],
+          [meterKey]: {},
+        },
+      }));
       setMeterCount(meterCount + 1);
     }
-    console.log(formData);
+    // console.log(formData);
   };
+
+  const handlePress = () => {
+    console.log(formData)
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -74,19 +103,18 @@ const selfReport = () => {
                 items={houseItems}
                 style={{
                   inputIOS: styles.pickerItem,
-                  inputAndroid: styles.pickerItem
+                  inputAndroid: styles.pickerItem,
                 }}
                 placeholder={{
                   label: "Избери имот",
-                  value: ""
+                  value: "",
                 }}
                 value={value}
+                
               />
             </View>
           </View>
-          <View style={styles.waterMeterContainer}>
-            {meters}
-          </View>
+          <View style={styles.waterMeterContainer}>{meters}</View>
           <Pressable
             onPress={addWaterMeter}
             onPressIn={() => setOpacity(0.5)}
@@ -96,6 +124,14 @@ const selfReport = () => {
               {getIcon("plus", "#3CA5D8", 15)}Добави водомер
             </Text>
           </Pressable>
+          <CustomButton
+            handlePress={handlePress}
+            color={"#388FED"}
+            secondColor={"#205187"}
+            title={"Добави"}
+            additionalStyles={{ width: "100%", height: 68, borderRadius: 20, padding: 0 }}
+            additionalTextStyle={{ fontSize: 20, textAlign: "center" }}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>

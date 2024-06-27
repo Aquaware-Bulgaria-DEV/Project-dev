d11de9cf552d8d9e6dd0572cdea834a448b22cbe
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-06-27 22:35:38 +0300

    Update customButton.jsx

--- frontend/app/globalComponents/customButton.jsx
+++ frontend/app/globalComponents/customButton.jsx
@@ -1,9 +1,10 @@
 import { TouchableOpacity, Text, StyleSheet, Image } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient';
 import React from 'react';
 
 import { styles } from "./customButtonStyles"
 
-const CustomButton = ({title, handlePress, isLoading, additionalStyles, imagePath, additionalTextStyle}) => {
+const CustomButton = ({title, handlePress, isLoading, additionalStyles, imagePath, additionalTextStyle, color, secondColor}) => {
   return (
     <TouchableOpacity
     onPress={handlePress}
@@ -12,7 +13,7 @@ const CustomButton = ({title, handlePress, isLoading, additionalStyles, imagePat
     disabled={isLoading}
     >
       
-      { imagePath ? <Image source={imagePath} /> :<Text style={[styles.btn, additionalTextStyle]}>{title}</Text>}
+      { imagePath ? <Image source={imagePath} /> : secondColor ? (<LinearGradient start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} style={styles.gradient} colors={[color, secondColor]}><Text style={[styles.btn, additionalTextStyle]}>{title}</Text></LinearGradient>) : (<Text style={[styles.btn, additionalTextStyle]}>{title}</Text>)}
     </TouchableOpacity>
   )
 }

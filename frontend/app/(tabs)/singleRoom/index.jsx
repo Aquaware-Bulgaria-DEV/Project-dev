7bfcb1c80907d6de8041b4262661ed8b514710ef
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-07-12 11:48:00 +0300

    Update index.jsx

--- frontend/app/(tabs)/singleRoom/index.jsx
+++ frontend/app/(tabs)/singleRoom/index.jsx
@@ -1,12 +1,15 @@
 // app/subscreens/singleRoom/index.jsx
 import React from 'react';
 import { View, Text, Button } from 'react-native';
-import { router } from 'expo-router';
+import { router, useLocalSearchParams } from 'expo-router';
 
-const SingleRoomView = () => {
+const SingleRoomView = ({route, navigation}) => {
+  const { id } = useLocalSearchParams();
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Single Room View</Text>
+      <Text>{id}</Text>
       <Button title="Go Back" onPress={() => router.back()} />
     </View>
   );

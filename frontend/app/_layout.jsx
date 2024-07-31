b6c66d6e08334834a9a9c4f58abffab30a59ee0f
Diana Mitkova <diana_guerdjilova@abv.bg>
2024-07-31 15:04:59 +0300

    Created switch buttons

--- frontend/app/_layout.jsx
+++ frontend/app/_layout.jsx
@@ -8,6 +8,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { AuthProvider } from './Context/AuthContext'; 
 import AuthContext from './Context/AuthContext';
 import { LanguageProvider } from '../src/context/LanguageContext';
+import { NotificationProvider } from '../src/context/NotificationsContext';
 
 // const MyStack = () => {
 

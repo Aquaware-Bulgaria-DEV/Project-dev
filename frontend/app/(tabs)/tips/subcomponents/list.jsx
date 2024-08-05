37eaf9dd6c02a429c6b3b0b861ab23fb4ce20a98
vaninaNina <kalapsazova.vanina@gmail.com>
2024-08-05 12:45:10 +0300

    fix router on the home button

--- frontend/app/(tabs)/tips/subcomponents/list.jsx
+++ frontend/app/(tabs)/tips/subcomponents/list.jsx
@@ -1,26 +1,27 @@
-import React from "react";
-import { useState } from "react";
-import { View, Text, ScrollView, Pressable } from "react-native";
-import AntDesignI from "react-native-vector-icons/AntDesign";
-import { styles } from "./subcompStyles.js";
+import React from 'react';
+import { useState } from 'react';
+import { View, Text, ScrollView, Pressable } from 'react-native';
+import AntDesignI from 'react-native-vector-icons/AntDesign';
+import { styles } from './subcompStyles.js';
 
-import { TipsByCategory } from "./tips.jsx";
+import { TipsByCategory } from './tips.jsx';
 
 import '../../../../src/i18n/i18n.config';
 import { useTranslation } from 'react-i18next';
+import { useRouter } from 'expo-router';
 
-const categories = ["МИВКА КУХНЯ", "ПЕРАЛНЯ", "ДУШ БАНЯ", "ТОАЛЕТНА"];
+const categories = ['МИВКА КУХНЯ', 'ПЕРАЛНЯ', 'ДУШ БАНЯ', 'ТОАЛЕТНА'];
 
 export const List = () => {
-
   const { t, i18n } = useTranslation();
 
+  const router = useRouter();
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
 
   const pressHandler = (category) => {
     setSelectedCategory(category);
     //TODO: onPress to scroll to the selected section
-    console.log("Selected category:", category);
+    console.log('Selected category:', category);
   };
 
   return (
@@ -49,16 +50,16 @@ export const List = () => {
 
       <Pressable
         style={styles.accountContainer}
-        onPress={() => console.log("pressed")}
+        onPress={() => router.push('subscreens/myProfile')}
       >
         <View style={styles.account}>
-          <AntDesignI name={"user"} size={25} style={styles.userIcon} />
+          <AntDesignI name={'user'} size={25} style={styles.userIcon} />
           <View>
             <Text style={styles.accountTitle}>{t('myAccount')}</Text>
             <Text style={styles.accountSubtitle}>{t('compare')}</Text>
           </View>
         </View>
-        <AntDesignI name={"arrowright"} size={35} style={styles.arrow} />
+        <AntDesignI name={'arrowright'} size={35} style={styles.arrow} />
       </Pressable>
 
       <TipsByCategory></TipsByCategory>

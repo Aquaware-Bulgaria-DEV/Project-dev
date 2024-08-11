beb8b052adfa0d7f140a2befb136ef2a57e13e8b
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-08-11 20:30:09 +0300

    Update index.jsx

--- frontend/app/subscreens/selfReport/index.jsx
+++ frontend/app/subscreens/selfReport/index.jsx
@@ -97,7 +97,7 @@ const selfReport = () => {
     const dateObj = new Date(dateString);
     
     const day = String(dateObj.getUTCDate()).padStart(2, '0');
-    const month = String(dateObj.getUTCMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+    const month = String(dateObj.getUTCMonth() + 1).padStart(2, '0'); 
     const year = dateObj.getUTCFullYear();
     
     return `${day}.${month}.${year}`;

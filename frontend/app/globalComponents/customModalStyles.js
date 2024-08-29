6a08499aff68fab4939f7427a27f885128adec4d
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-08-29 19:44:54 +0300

    Update customModalStyles.js

--- frontend/app/globalComponents/customModalStyles.js
+++ frontend/app/globalComponents/customModalStyles.js
@@ -22,6 +22,32 @@ export const styles = StyleSheet.create({
       shadowRadius: 4,
       elevation: 5
     },
+    yesButton: {
+      backgroundColor: "#fff", 
+      width: 50,
+      padding: 10,
+      borderRadius: 5,
+      borderWidth: 2,
+      borderColor: '#FF5C5C',
+    },
+    noButton: {
+      backgroundColor: "#fff", 
+      width: 50,
+      padding: 10,
+      borderColor: "#CCC",
+      borderWidth: 2,
+      borderRadius: 5
+    },
+    buttonTextYes: {
+      color: "#C7253E", 
+      fontWeight: "bold",
+      textAlign: "center",
+    },
+    buttonTextNo: {
+      color: "#686D76", 
+      fontWeight: "bold",
+      textAlign: "center"
+    },
     modalText: {
       fontSize: 18,
       textAlign: "center"

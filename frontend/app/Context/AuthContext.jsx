25c5ff5ec7859d01d987cd5a53c1a1bf67763180
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-09-01 16:22:02 +0300

    chore: logout functionality and useAuth context rdy to use

--- frontend/app/Context/AuthContext.jsx
+++ frontend/app/Context/AuthContext.jsx
@@ -1,5 +1,6 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useState, useEffect, useContext } from 'react';
 import { storeData, getData, removeData } from '../../utils/storage';
+import { useRouter } from 'expo-router';
 
 const AuthContext = createContext();
 
@@ -7,6 +8,7 @@ export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(null);
   const [preferences, setPreferences] = useState(null);
   const [userInfo, setUserInfo] = useState(null);
+  const router = useRouter();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -51,6 +53,13 @@ export const AuthProvider = ({ children }) => {
     setUserInfo(null);
   };
 
+  const logout = async () => {
+    await removeToken();
+    await removeUserInfo();
+    router.dismissAll();
+    router.push("(auth)/signIn");
+  }
+
   return (
     <AuthContext.Provider
       value={{
@@ -63,11 +72,13 @@ export const AuthProvider = ({ children }) => {
         removePreferences,
         saveUserInfo,
         removeUserInfo,
+        logout
       }}
     >
       {children}
     </AuthContext.Provider>
   );
 };
+export const useAuth = () => useContext(AuthContext);
 
 export default AuthContext;

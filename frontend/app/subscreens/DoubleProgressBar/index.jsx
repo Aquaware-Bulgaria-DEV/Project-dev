f2a9f7865bd6e4e452d135fbe9dc400dacf14e60
Diana Mitkova <diana_guerdjilova@abv.bg>
2024-09-05 16:07:45 +0300

    Added Translation to Double Progress Bar page

--- frontend/app/subscreens/DoubleProgressBar/index.jsx
+++ frontend/app/subscreens/DoubleProgressBar/index.jsx
@@ -9,8 +9,10 @@ import CircularProgressBar from '../../globalComponents/progressBar';
 
 import FishTank from "../../../assets/Fish.png"
 import BathTub from "../../../assets/Bathtub.png"
+import { useTranslation } from 'react-i18next'
 
 const DoubleProgressBar = () => {
+  const { t } = useTranslation();
   const { progressPercent, currentQuantity, errMsg } = useLocalSearchParams();
   const litersQuantity = currentQuantity * 1000;
 
@@ -30,7 +32,7 @@ const DoubleProgressBar = () => {
             {/* Conditionally render textContainer only if there is no error message */}
             {!errMsg && (
               <View style={styles.textContainer}>
-                <Text style={styles.quantity}>{litersQuantity} литра</Text>
+                <Text style={styles.quantity}>{litersQuantity} {t("doubleProgressBarLiters")}</Text>
                 <Text style={styles.tip}>колкото един аквариум в хотел</Text>
               </View>
             )}
@@ -43,13 +45,13 @@ const DoubleProgressBar = () => {
             />
           </View>
 
-          <Text style={styles.quantity}>или</Text>
+          <Text style={styles.quantity}>{t("doubleProgressBarOr")}</Text>
 
           <View style={styles.subContainer}>
             {/* Conditionally render textContainer only if there is no error message */}
             {!errMsg && (
               <View style={styles.textContainer}>
-                <Text style={styles.quantity}>{litersQuantity} литра</Text>
+                <Text style={styles.quantity}>{litersQuantity} {t("doubleProgressBarLiters")}</Text>
                 <Text style={styles.tip}>колкото една почти пълна вана</Text>
               </View>
             )}

1ac3b5cd39d384f9791d6b2215856b12eda769fd
SunflowerTeaDev <62473184+SunflowerTeaDev@users.noreply.github.com>
2024-09-21 19:23:01 +0300

    Delete frontend_backup directory

--- frontend_backup/app/globalStyles.js
+++
@@ -1,9 +0,0 @@
-<link href="https://fonts.cdnfonts.com/css/muli" rel="stylesheet"></link>
-
-const primaryColor = 'rgb(0, 122, 255)';
-// const primaryFont = "'Muli', sans-serif";
-
-export default globalStyles = {
-    primaryColor,
-    // primaryFont,
-}
--- frontend_backup/src/i18n/translations/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as bg } from './bg.json';
-export { default as en } from './en.json';
